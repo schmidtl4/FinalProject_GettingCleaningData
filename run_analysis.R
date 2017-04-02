bffb1a0fc8a6e89151bdfc242d786dd145b68fd6
Les <les242xyz@casadiablo.net>
2017-04-01 21:51:05 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -3,13 +3,13 @@
 library(readr)
 library(dplyr)
 
-##download and unzip the necessary data into a folder called "projectData" 
+## Step 1: download and unzip the necessary data into a folder called "projectData" 
 ## in the working directory
 url="https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(url,"projectData.zip")
 unzip("projectData.zip",exdir = "projectData")
 
-##read the various data sets
+## Step 2: Load/read the various data sets into R
 testx <- read.csv(
     "~/R_Projects/projectData/UCI HAR Dataset/test/X_test.txt",
     sep="",header=FALSE)
@@ -28,19 +28,15 @@ trainy <- read.csv(
 trainsubject <- read.csv(
     "~/R_Projects/projectData/UCI HAR Dataset/train/subject_train.txt",
     sep="",header=FALSE)
-
-#read the list of feature names
 features <- read.csv(
     "~/R_Projects/projectData/UCI HAR Dataset/features.txt",
     sep="",header=FALSE)
-
-#read the activity labels 
 activitylabels <- read.csv(
     "~/R_Projects/projectData/UCI HAR Dataset/activity_labels.txt",
     sep="",header=FALSE)
 
 
-##Step 1: Merge training and test data sets into one
+##Step 3: Merge training and test data sets into one
 ##testx,testy and testsubject are randomly selected data from the total population (30%)
 ##trainx,trainy and trainsubject are randomly selected data from the total population (70%)
 ##this step recombines the two parts into one unified set (i.e. the total population)
@@ -48,8 +44,7 @@ activitylabels <- read.csv(
     ally <-rbind(testy,trainy)
     allsubject <-rbind(testsubject,trainsubject)
     
-
-##Step 2: extract the mean and standard deviation for each measurement
+##Step 4: Select relevant columns: extract the mean and standard deviation for each measurement.  Additionally, combine the activity and subject identifiers to the data and label appropriate
 
     #label columns in allx to allow proper extraction
     names(allx) <- features$V2
@@ -69,7 +64,7 @@ activitylabels <- read.csv(
     names(dffinal)[2] <- 'activity'
     
 
-##Step 3: name the activities in the data set
+##Step 5: name the activities in the data set
     dffinal$activity <- activitylabels[,2][match(dffinal$activity,activitylabels[,1])]
 
 ##Step 4: label the data set with descriptive variable names
